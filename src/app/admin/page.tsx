a674b5deefc7a546d138cb5bed1ffbae49f51a33
arjun tamang <tamangarjun99841@gmail.com>
2024-05-05 22:15:26 +0545

    fix

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,7 +2,7 @@
 import Dashboard from '@/components/Dashbord/Dashboard'
 import WithProtectedRoute from '@/components/ProtectedComponent'
 
-import Sidebar from '@/components/common/Sidebar'
+
 import React from 'react'
 
 function Page() {
